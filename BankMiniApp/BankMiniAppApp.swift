2fdd4675d7221ad54416631fd549a21bb8156768
Nastya <mail@yuhimenko-anastasia.ru>
2025-12-04 11:22:00 +0300

    Initial Commit

---
+++ BankMiniApp/BankMiniAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  BankMiniAppApp.swift
+//  BankMiniApp
+//
+//  Created by Anastasia on 04.12.2025.
+//
+
+import SwiftUI
+
+@main
+struct BankMiniAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ BankMiniAppTests/BankMiniAppTests.swift
@@ -0,0 +1,17 @@
+//
+//  BankMiniAppTests.swift
+//  BankMiniAppTests
+//
+//  Created by Anastasia on 04.12.2025.
+//
+
+import Testing
+@testable import BankMiniApp
+
+struct BankMiniAppTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
